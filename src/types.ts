fa7a494a69aced7d78cd024133a89410f432adef
Waleed Gudah <aquataze@yahoo.com>
2024-05-30 12:54:37 -0500

    Update dependencies, refactor fetchItchGameData, add exampleUsage, and type adjustments

--- src/types.ts
+++ src/types.ts
@@ -1,40 +1,39 @@
 export interface GameMetadata {
-	gameId: number;
-	title: string;
-	description: string;
-	coverImage: string;
-	gamePage: string;
-	comments: string;
-    metaDataUrl: string;
-	latestVersion: {
-		displayName: string;
-		versionId: number;
-		releaseDate: Date;
-		sizeBytes: number;
-		sizeMB: number;
-		downloadUrl: string;
-		md5Hash: string;
-		platforms: {
-			windows: boolean;
-			mac: boolean;
-			linux: boolean;
-			android: boolean;
-		};
-		preorder: boolean;
-		position: number;
-		filename: string;
-		demo: boolean;
-		createdAt: Date;
-		storage: string;
-		type: string;
-	};
-	authors: Array<{ name: string; url: string }>;
+  gameId: number;
+  title: string;
+  description: string;
+  coverImage: string;
+  gamePage: string;
+  comments: string;
+  metaDataUrl: string;
+  latestVersion: {
+    displayName: string;
+    versionId: number;
+    releaseDate: Date;
+    sizeBytes: number;
+    sizeMB: number;
+    downloadUrl: string;
+    md5Hash: string;
+    platforms: {
+      windows: boolean;
+      mac: boolean;
+      linux: boolean;
+      android: boolean;
+    };
+    preorder: boolean;
+    position: number;
+    filename: string;
+    demo: boolean;
+    createdAt: Date;
+    storage: string;
+    type: string;
+  };
+  authors: Array<{ name: string; url: string }>;
 }
 
 export interface FetchGameDataParams {
-    itchApiKey?: string;
-    author?: string;
-    gameTitle?: string;
-    gameUrl?: string;
-  }
-  
+  itchApiKey?: string;
+  author?: string;
+  gameTitle?: string;
+  gameUrl?: string;
+}
