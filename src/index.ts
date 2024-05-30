5a4069c6a963cf4dfb9a224e4e92c3014720ba3b
Waleed Gudah <aquataze@yahoo.com>
2024-05-30 13:10:37 -0500

    Removed .npmignore, updated package and lock files, modified src/index.ts, and added .npmrc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import * as dotenv from "dotenv";
 export { fetchItchGameData } from "./fetchItchGameData";
 export { FetchGameDataParams, GameMetadata } from "./types";
-
-dotenv.config({ path: ".env.local" });
